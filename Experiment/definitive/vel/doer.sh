841f12ef4288bc52eddc09917feb971df74c7c9d
Iximiel <iximiel@gmail.com>
2016-05-07 15:15:20 +0200

    cambio capitolo errore e velocita`

--- Experiment/definitive/vel/doer.sh
+++ Experiment/definitive/vel/doer.sh
@@ -3,9 +3,19 @@
 pot=free.set
 prog=../../../single
 
-for Mass in 10 20
+for Mass in 5 10 20
 do
     settings=msm$Mass.set
+    #    for sigma in 05 1 2 4
+    #    do
+    #	onda=gauss$sigma.set
+    for E in 10 25 50 75 100 150 200
+    do
+	onda=g4_E$E.set
+	echo $prog $pot $onda $settings
+	$prog $pot $onda $settings
+    done
+    settings=m$Mass.set
     #    for sigma in 05 1 2 4
     #    do
     #	onda=gauss$sigma.set
