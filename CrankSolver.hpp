4160f1da98261f510e7442c0429d1327ced1a3a7
Iximiel <iximiel@gmail.com>
2016-05-17 16:08:03 +0200

    errore

---
+++ CrankSolver.hpp
@@ -0,0 +1,21 @@
+#ifndef _Crank
+#define _Crank
+#include "Tridiag.hpp"
+
+//NB: questo solver non e` generalizzato per CC e V variabili nel tempo
+class CrankSolver{
+public:
+  CrankSolver(tridiag *mat, int Ns);
+  ~CrankSolver();
+  int doStep();
+  void SetInitialState(Var* );
+  Var getPoint(int x);
+private:
+  void Stepper();
+  Var* CS_data;
+  int CS_ns;
+  int CS_step;
+  tridiag *CS_mat;
+};
+
+#endif //_Cranck
